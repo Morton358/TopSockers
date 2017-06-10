81211f6acd1e7c42152c5875322e3cbdbfff047a
Morton358 <vovankovalchuk358@gmail.com>
2017-06-10 18:32:42 +0200

    define iterator

--- zad.js
+++ zad.js
@@ -11,12 +11,13 @@ function loadStrzelcy() {
   xhttp.open("GET", "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/topscorers", true);
   xhttp.setRequestHeader("X-Mashape-Key", "kxSXmUymofmshFHhhKxWOSJpqJsJp1I3zNnjsnqKwhITAiC1zw");
   xhttp.send();
-}
+};
 
 function myFunc(jsonStr) {
   var obj = JSON.parse(jsonStr);
   var elements = obj.data.topscorers;
   var table = "<tr><th>Pozycja</th><th>Zawodnik</th><th>Gole</th></tr>";
+  var i = 0;
   for (i = 0; i < elements.length; i++){
     var strzelca = elements[i];
     table += "<tr><td>" + String(i + 1) + "</td><td>" + strzelca.fullname + "</td><td>" + strzelca.goals + "</td></tr>";
